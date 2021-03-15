06ca177972de28668efe1bde8b6cb921d3d12ec2
kennyfmx <fmxhighlife@gmail.com>
2021-03-15 16:31:30 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let myConfig = bracketsConfig.map(el => el.join(''));
+    let middle = str.length / 2;
+    for (let i = 0; i < middle; i++) {
+		for (let a = 0; a < myConfig.length; a++) {
+            str = str.replace(myConfig[a], "");
+        }
+	}
+	return (str.length === 0);
 }
